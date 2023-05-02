f3d867865612496fe130e55228de1fbbda192ddf
Adam Derewecki <a@der.wiki>
2023-05-02 12:25:05 -0700

    Remove hardcoded values to generalize; make invokable from command line

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import openai
 import pandas as pd
 import numpy as np
@@ -5,28 +7,44 @@ import os
 from openai.embeddings_utils import get_embedding
 from openai.embeddings_utils import cosine_similarity
 
+from sentence_formatter import text_to_sentence_csv
+
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-# use sentence_formatter to generate CSV from text
-df = pd.read_csv('chapter1-sunset.csv')
 
-if not os.path.exists('word_embeddings.csv'):
-    print('generating embeddings for book')
-    df['embedding'] = df['text'].apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
-    df.to_csv('word_embeddings.csv')
+def semantic_search(text_filename: str, search_term: str):
+    if not text_filename.endswith('.txt'):
+        raise Exception(f'Invalid input text file: {text_filename}')
 
-print('loading embeddings')
-df = pd.read_csv('word_embeddings.csv')
-df['embedding'] = df['embedding'].apply(eval).apply(np.array)
+    # use sentence_formatter to generate CSV from text
+    csv_filename = text_to_sentence_csv(text_filename)
+    df = pd.read_csv(csv_filename)
+    text_file_prefix = text_filename.replace('.txt', '')
+    embeddings_filename = f'{text_file_prefix}_word_embeddings.csv'
 
-search_term = "romantic relationships ending"
-print(f'getting embeddings for search term: {search_term}')
-search_term_vector = get_embedding(search_term, engine="text-embedding-ada-002")
+    if not os.path.exists(embeddings_filename):
+        print('generating embeddings for book')
+        df['embedding'] = df['text'].apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
+        df.to_csv(embeddings_filename)
 
-print('finding sentences similar to search term')
-df["similarities"] = df['embedding'].apply(lambda x: cosine_similarity(x, search_term_vector))
+    print('loading embeddings')
+    df = pd.read_csv(embeddings_filename)
+    df['embedding'] = df['embedding'].apply(eval).apply(np.array)
 
-print('top 20 results:')
-print(df.sort_values("similarities", ascending=False).head(20))
+    print(f'getting embeddings for search term: {search_term}')
+    search_term_vector = get_embedding(search_term, engine="text-embedding-ada-002")
 
-# TODO take output and prompt chatgpt, "Given the sentence, extract the 'answer' for the search term"
+    print('finding sentences similar to search term')
+    df["similarities"] = df['embedding'].apply(lambda x: cosine_similarity(x, search_term_vector))
+
+    print('top 20 results:')
+    print(df.sort_values("similarities", ascending=False).head(20))
+
+    # TODO take output and prompt chatgpt, "Given the sentence, extract the 'answer' for the search term"
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        raise Exception("Missing required params")
+
+    semantic_search(text_filename=sys.argv[1], search_term=sys.argv[2])
--- sentence_formatter.py
+++ sentence_formatter.py
@@ -1,27 +1,44 @@
 import csv
+import os.path
 import re
 
 from nltk.tokenize import sent_tokenize
 import nltk
 
-print('Download the required NLTK data')
-nltk.download('punkt')
 
-print('Read the ebook text file')
-with open('chapter1-sunset.txt', 'r', encoding='utf-8') as file:
-    text = file.read()
+def text_to_sentence_csv(input_text_file: str) -> str:
+    """
+    :param input_text_file:
+    :return: string value of the path for the output CSV file
+    """
+    if not os.path.exists(input_text_file):
+        raise Exception(f'File not found: {input_text_file}')
 
-print('Discard superfluous line breaks')
-text = re.sub(r'\n(?:\s*\n)*(?=[a-z])', ' ', text)
-# text = re.sub(r'\n(?=[a-z])', ' ', text)
+    output_csv_filename = input_text_file.replace(".txt", ".csv")
+    if os.path.exists(output_csv_filename):
+        print('CSV already exists, reusing)')
+        return output_csv_filename
 
-print('Tokenize the text into sentences')
-sentences = sent_tokenize(text)
+    print('Download the required NLTK data')
+    nltk.download('punkt')
 
-print('Write the sentences to a CSV file')
-with open('chapter1-sunset.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    csvfile.write('text\n')
-    for sentence in sentences:
-        # sentences that set up dialog have newlines; strip them
-        writer.writerow([sentence.replace('\n', ' ')])
+    print('Read the ebook text file')
+    with open(input_text_file, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    print('Discard superfluous line breaks')
+    text = re.sub(r'\n(?:\s*\n)*(?=[a-z])', ' ', text)
+    # text = re.sub(r'\n(?=[a-z])', ' ', text)
+
+    print('Tokenize the text into sentences')
+    sentences = sent_tokenize(text)
+    print(f'Found {len(sentences)} sentences')
+
+    print('Write the sentences to a CSV file')
+    with open(output_csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        csvfile.write('text\n')
+        for sentence in sentences:
+            # sentences that set up dialog have newlines; strip them
+            writer.writerow([sentence.replace('\n', ' ')])
+    return output_csv_filename
